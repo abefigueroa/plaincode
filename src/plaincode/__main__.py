c862e7bd14ce5d92f9811da9c428dfe1f2eeda6e
Abraham Figueroa <abraham.figueroa06@gmail.com>
2026-09-07 21:11:34 -0600

    Add terminal menu selection

--- src/plaincode/__main__.py
+++ src/plaincode/__main__.py
@@ -11,6 +11,15 @@ def main() -> None:
     print("2. Plain English to Python")
     print("3. Exit")
 
+    choice = input("Choose an option: ")
+    if choice == "1":
+        print("Python to plain English selected.")
+    elif choice == "2":
+        print("Plain English to Python selected.")
+    elif choice == "3":
+        print("Goodbye!")
+    else:
+        print("invalid option.")
 
 if __name__ == "__main__":
     main()
