5b7f3f3572feff52b48869ac710b1c1eec9d4762
Abraham Figueroa <abraham.figueroa06@gmail.com>
2026-09-07 21:00:56 -0600

    Set up PlainCode terminal application

---
+++ src/plaincode/__main__.py
@@ -0,0 +1,16 @@
+"""Application entry point and workflow."""
+
+
+# Functions
+
+
+def main() -> None:
+    """Run the PlainCode terminal application."""
+    print("PlainCode")
+    print("1. Python to plain English")
+    print("2. Plain English to Python")
+    print("3. Exit")
+
+
+if __name__ == "__main__":
+    main()
